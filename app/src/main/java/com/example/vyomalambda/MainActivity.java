544fad394cb5ec0e8d6d9c277a94786242c598fe
Sudhir Patavardhan <patavardhan@gmail.com>
2020-04-19 16:43:46 +0530

    Signup and menu Added
    Signed-off-by: Sudhir Patavardhan <patavardhan@gmail.com>

--- app/src/main/java/com/example/vyomalambda/MainActivity.java
+++ app/src/main/java/com/example/vyomalambda/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.vyomalambda;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,14 +30,53 @@ public class MainActivity extends AppCompatActivity {
 
     private void GoHome()
     {
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                switch (userStateDetails.getUserState()) {
+                    case SIGNED_IN:
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                runOnUiThread(new Runnable() {
+                                    public void run() {
+                                        setContentView(R.layout.activity_main);
+                                    }
+                                });
+                            }
+                        });
+                        break;
+                    case SIGNED_OUT:
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                GoSignIn();
+                            }
+                        });
+                        break;
+
+                    default:
+                        AWSMobileClient.getInstance().signOut();
+                        break;
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("INIT", e.toString());
+            }
+        });
+    }
+
+    private void GoSignUp() {
         runOnUiThread(new Runnable() {
             public void run() {
 
-                setContentView(R.layout.activity_main);
-                ImageButton btnSignout = findViewById(R.id.btnSignout);
-                btnSignout.setOnClickListener(new View.OnClickListener() {
+                setContentView(R.layout.activity_signup);
+                final Button btnSignUp = findViewById(R.id.btnSignup);
+                btnSignUp.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
-                        Signout();
+                        Signup();
                     }
                 });
             }
@@ -47,11 +89,19 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 setContentView(R.layout.activity_signin);
 
-                final Button button = findViewById(R.id.btnSignIn);
+                final Button buttonSignIn = findViewById(R.id.btnSignIn);
 
-                button.setOnClickListener(new View.OnClickListener() {
+                buttonSignIn.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
-                        Signin(getTextVal(R.id.txtSignInName), getTextVal(R.id.txtSigninPassword));
+                        Signin("+91" + getTextVal(R.id.txtSignInName), getTextVal(R.id.txtSigninPassword));
+                    }
+                });
+
+                final Button buttonSignUp = findViewById(R.id.btnGoSignUp);
+
+                buttonSignUp.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        GoSignUp();
                     }
                 });
             }
@@ -111,31 +161,26 @@ public class MainActivity extends AppCompatActivity {
                 //Log.e("Error", "Sign-in error", e);
                 String msg = e.getMessage();
                 makeToast(msg.substring(0, msg.indexOf("(") ));
-                /*
-                setContentView(R.layout.activity_);
-
-                //sign up
-
-                final Button button = findViewById(R.id.btnSignin);
-
-                button.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        Signup();
-                    }
-                });
-
-                 */
-
             }
         });
     }
 
     private void Signup()
     {
-        final String username = ((EditText)findViewById(R.id.txtSignupUsername)).getText().toString();
+        final String username = "+91" + ((EditText) findViewById(R.id.txtSignupUsername)).getText().toString();
         final String password = ((EditText)findViewById(R.id.txtSignupPassword)).getText().toString();
+        final String cPpassword = ((EditText) findViewById(R.id.txtSignupCPassword)).getText().toString();
+        //
+        final String emailId = ((EditText) findViewById(R.id.txtSignupEmail)).getText().toString();
+        //
+        if (!password.equals(cPpassword)) {
+            makeToast("Passwords don't match.");
+            return;
+        }
+
         final Map<String, String> attributes = new HashMap<>();
-        attributes.put("email", "name@email.com");
+        attributes.put("custom:subscription", "free");
+        attributes.put("email", emailId);
         AWSMobileClient.getInstance().signUp(username, password, attributes, null, new Callback<SignUpResult>() {
             @Override
             public void onResult(final SignUpResult signUpResult) {
@@ -149,6 +194,7 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             makeToast("Sign-up done.");
                         }
+                        GoSignIn();
                     }
                 });
             }
@@ -156,6 +202,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError(Exception e) {
                 Log.e("Sign-up error", String.valueOf(e));
+                String msg = e.getMessage();
+                makeToast(msg.substring(0, msg.indexOf("(")));
             }
         });
     }
@@ -167,6 +215,7 @@ public class MainActivity extends AppCompatActivity {
             int duration = Toast.LENGTH_LONG;
             public void run() {
                 Toast toast = Toast.makeText(context, s, duration);
+                toast.setGravity(Gravity.CENTER, 0, 0);
                 toast.show();
             }
         });
@@ -174,53 +223,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Events
-
-        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
-            @Override
-            public void onResult(UserStateDetails userStateDetails) {
-                switch (userStateDetails.getUserState()){
-                    case SIGNED_IN:
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                GoHome();
-                            }
-                        });
-                        break;
-                    case SIGNED_OUT:
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Log.e("applog",getApplicationContext().toString());
-                                GoSignIn();
-                            }
-                        });
-                        break;
-
-                    default:
-                        AWSMobileClient.getInstance().signOut();
-                        break;
-                }
-            }
-
-            @Override
-            public void onError(Exception e) {
-                Log.e("INIT", e.toString());
-            }
-        });
-
-
-
+        GoHome();
     }
 
     private String getTextVal(int viewId) {
         return ((EditText)findViewById(viewId)).getText().toString();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                GoHome();
+                return true;
+            case R.id.menu_logout:
+                Signout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
